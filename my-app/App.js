654e81ea51202b27bc82c3128eacc7ab208e0e4a
ingname <81479367+ingname@users.noreply.github.com>
2023-05-10 15:30:06 +0300

    d

---
+++ my-app/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <Text style={styles.h1}>Focus</Text>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  h1: {
+
+    width: 106,
+    height: 44,
+    left: 127,
+    top: 20,
+  }
+})
+
+export default App

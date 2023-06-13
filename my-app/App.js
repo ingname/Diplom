22c85c5be58075fe4a31db2d2b38720f48122fbe
Alexander <79362302+E314-byte@users.noreply.github.com>
2023-06-13 20:55:20 +0300

    1.0

--- my-app/App.js
+++ my-app/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import { useFonts } from 'expo-font';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ProfileScreen from './src/nav/Autorization/auth';
-import HomeScreen from'./src/nav/hello_window';
+import HomeScreen from './src/nav/hello_window';
 import MainScreen from './src/nav/Main/Main_window'
 import Registration from './src/nav/Autorization/reg';
 import SudokuScreen from './src/nav/Game/Sudoku_window';
@@ -14,7 +14,7 @@ import SudokuScreen from './src/nav/Game/Sudoku_window';
 
 const Stack = createNativeStackNavigator();
 
-export default function App () {
+export default function App() {
 
   const [fontsLoaded] = useFonts({
     'OpenSansBold': require('./assets/fonts/OpenSansBold.ttf'),
@@ -26,42 +26,43 @@ export default function App () {
 
   return (
     <NavigationContainer>
-     <Stack.Navigator>
+      <Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{headerShown: false}}
-          
+          options={{ headerShown: false }}
+
         />
         <Stack.Screen
           name="MainScreen"
           component={MainScreen}
-          options={{headerShown: false}}
-          
-        />
-        <Stack.Screen 
-          name="Profile" 
-          component={ProfileScreen} 
-          options={{headerShown: false
-            }}
-          
-        />
-        {/* <Stack.Screen 
-          name="MainScreen" 
-          component={MainScreen} 
-          options={{headerShown: false}}
-          
-        /> */}
-        <Stack.Screen 
-          name="Registration" 
-          component={Registration} 
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
 
         />
-        <Stack.Screen 
-          name="Sudoku" 
-          component={SudokuScreen} 
-          options={{headerShown: false}}
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            headerShown: false
+          }}
+
+        />
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={{ headerShown: false }}
+
+        />
+        <Stack.Screen
+          name="Registration"
+          component={Registration}
+          options={{ headerShown: false }}
+
+        />
+        <Stack.Screen
+          name="Sudoku"
+          component={SudokuScreen}
+          options={{ headerShown: false }}
 
         />
       </Stack.Navigator>
